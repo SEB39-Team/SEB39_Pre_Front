c149f56360759c660b96039193a8da33d7e459b9
Kyoorim <kyoorim88@gmail.com>
2022-08-25 16:22:50 +0900

    Feat: 로그인화면 디자인 구현

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,3 +1,39 @@
-export default function Login() {
-  return <div>hi</div>;
-}
+import styled from 'styled-components';
+
+import LoginForm from './LoginForm';
+
+import Logo from '../../assets/logo-stackoverflow.svg';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100vw;
+`;
+
+const Text = styled.div`
+  margin-top: 20px;
+  font-size: 15px;
+
+  span:nth-child(2) {
+    color: var(--blue);
+    cursor: pointer;
+  }
+`;
+
+const Login = () => {
+  return (
+    <Wrapper>
+      <img src={Logo} alt="stackoverflow logo" height={37} />
+      <LoginForm />
+      <Text>
+        <span>{"Don't have an account ?"}</span>
+        <span> Sign Up</span>
+      </Text>
+    </Wrapper>
+  );
+};
+
+export default Login;

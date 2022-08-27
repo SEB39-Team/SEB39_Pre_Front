3f9cdfaaeacc2e768eb1c9af62d06a1d775572e6
devfrank9 <devfrank9@gmail.com>
2022-08-28 01:34:58 +0900

    Feat: "푸터 컴포넌트"

---
+++ src/components/Footer/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 30vh;
+  background-color: black;
+  font-size: 20vmin;
+  color: white;
+  z-index: 999;
+`;

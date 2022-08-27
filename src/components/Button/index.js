4b17787b723293e105d212e27d6ac16f7e5448f3
devfrank9 <devfrank9@gmail.com>
2022-08-27 23:39:05 +0900

    Refactor: "style 분리"

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,26 +1,10 @@
-import styled from 'styled-components';
-
-const ButtonComponent = styled.button`
-  font: inherit;
-  margin-top: 10px;
-  border: 1px solid var(--blue);
-  background: var(--blue);
-  color: white;
-  padding: 0.75rem 3.5rem;
-  cursor: pointer;
-  font-size: 13px;
-  font-weight: 600;
-  border-radius: 3px;
-  box-shadow: rgba(255, 255, 255, 0.4) 0px 1px 0px 0px inset;
-  height: 37px;
-  text-align: center;
-`;
+import * as S from './style';
 
 const Button = (props) => {
   return (
-    <ButtonComponent type={props.type || 'button'}>
+    <S.ButtonComponent type={props.type || 'button'}>
       {props.children}
-    </ButtonComponent>
+    </S.ButtonComponent>
   );
 };
 

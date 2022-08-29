c66f8e6bf81f01412383637108737863db36d1ee
Kyoorim <kyoorim88@gmail.com>
2022-08-29 10:47:50 +0900

    Feat:회원가입, 로그인화면에 TopNav 삽입

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import LoginForm from 'components/LoginForm';
 import Logo from 'assets/imgs/logo.svg';
 import * as S from './style';
+import { TopNav } from 'components';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,14 +10,17 @@ const Login = () => {
     navigate('/signup');
   };
   return (
-    <S.Wrapper>
-      <img src={Logo} alt="stackoverflow logo" height={37} />
-      <LoginForm />
-      <S.Text>
-        <span>{"Don't have an account ?"}</span>
-        <span onClick={directToSignUp}> Sign Up</span>
-      </S.Text>
-    </S.Wrapper>
+    <>
+      <TopNav></TopNav>
+      <S.Wrapper>
+        <img src={Logo} alt="stackoverflow logo" height={37} />
+        <LoginForm />
+        <S.Text>
+          <span>{"Don't have an account ?"}</span>
+          <span onClick={directToSignUp}> Sign Up</span>
+        </S.Text>
+      </S.Wrapper>
+    </>
   );
 };
 
